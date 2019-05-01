f218269316c0b58e77e9665ba25789c04e5e5358
cliftownh <cliftonhefley@gmail.com>
2019-04-30 17:36:55 -0700

    Temporary thumbnails

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const data = require('./data.json').projects;
+const path = require('path');
+
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+// const aboutRoutes = require('./routes/about');
+// const projRoutes = require('./routes/project');
+
+app.use(mainRoutes);
+// app.use('/about', aboutRoutes);
+// app.use('/project', projRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+  
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+  
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+});
+  
